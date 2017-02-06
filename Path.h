f5d006992e9cf1cf97b1b956953dceff63f4bb56
richard schorrig <richard.schorrig@web.de>
2017-02-06 03:46:04 +0100

    changes to buttons, shiftregisteroperation, to be tested

---
+++ Path.h
@@ -0,0 +1,22 @@
+/**
+ * Path.h
+ *
+ * Created on: 03.02.2017
+ * by Richard
+ */
+ 
+#ifndef PATH_H_
+#define PATH_H_
+
+#define DISPLAY_COMMANDRECEIVER
+
+#ifdef DISPLAY_COMMANDRECEIVER
+#define PATH_RSOSDEFINES_H "I:/CCS_ProjectsV6/Display_CommandReceiver/RSOSDefines.h"
+#define PATH_DEFINES_H "I:/CCS_ProjectsV6/Display_CommandReceiver/defines.h"
+#ifndef __MSP430G2553__
+#define __MSP430G2553__
+#endif /* __MSP430G2553__ */
+#endif /* DISPLAY_COMMANDRECEIVER */
+
+
+#endif /* PATH_H_ */
